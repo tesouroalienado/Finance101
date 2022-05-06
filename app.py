f8e15dc529f3f2461233b04b66d2b681a0103398
tesouroalienado <104900528+tesouroalienado@users.noreply.github.com>
2022-05-07 00:27:10 +0100

    update

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print("hello")
+import datetime
+
+import investpy
+
+search_result = investpy.search_quotes(text='apple', products=['stocks'],
+                                       countries=['united states'], n_results=1)
+recent_data = search_result.retrieve_recent_data()
+
+df = investpy.funds.search_funds(by='isin',value='LU0913601281')
+
+print(df['name'])
