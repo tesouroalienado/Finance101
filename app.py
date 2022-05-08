8a5dd268f3f7d45220ddc22b8038dbc0b3382154
tesouroalienado <104900528+tesouroalienado@users.noreply.github.com>
2022-05-08 15:25:28 +0100

    updated code

--- app.py
+++ app.py
@@ -1,11 +1,39 @@
 import datetime
-
+import streamlit as st
 import investpy
+import pandas as pd
+from streamlit_autorefresh import st_autorefresh
 
-search_result = investpy.search_quotes(text='apple', products=['stocks'],
-                                       countries=['united states'], n_results=1)
-recent_data = search_result.retrieve_recent_data()
+# update every 5 mins
 
-df = investpy.funds.search_funds(by='isin',value='LU0913601281')
+st.set_page_config(
+    page_title="Real-Time Score Dashboard",
+    page_icon="✅",
+    layout="wide",
+)
 
-print(df['name'])
+df_portfolio = pd.read_csv('portfolio.csv', header=0)
+
+data = []
+
+date_buy = st.date_input('date_buy')
+name = st.text_input('Name')
+quantity = st.number_input('Quantity', min_value=1, step=1)
+isin = st.text_input('isin')
+
+data.append([date_buy, name, quantity, isin])
+
+if st.button('Add to portfolio'):
+    pd.DataFrame(data).to_csv('portfolio.csv', mode='a')
+    st.write('Added to portfolio')
+    st_autorefresh(interval=1000, limit=2, key="dataframerefresh")
+else:
+    st.write('CLick to add to portfolio')
+
+st.dataframe(df_portfolio)
+
+df = investpy.stocks.get_stock_recent_data(stock='AAPL', country='United States')
+print(df)
+
+
+
